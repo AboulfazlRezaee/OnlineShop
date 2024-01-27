13f8277a45abbec69dfe3cddd44212f8ddb998ab
AboulfazlRezaee <rezaee.aboulfazl@gmail.com>
2024-01-27 23:09:43 +0330

    settings for media of the products models

--- products/models.py
+++ products/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.PositiveIntegerField(default=50000)
+    image = models.ImageField(verbose_name=_('Product Image'), upload_to='product/product_cover', blank=True,)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
